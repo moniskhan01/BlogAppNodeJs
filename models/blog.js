c961e6a728d21327ff54e9a7dba2ab96e12d4ab5
Monis Khan <khanmonis835@gmail.com>
2024-08-22 01:15:41 +0530

    blog app

---
+++ models/blog.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const { schema } = require("./user");
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    converImageURL: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = model("blog", blogSchema);
+
+module.exports = Blog;
